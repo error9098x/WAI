e9f79303103cf87649a6167a411ecd93db5d70ee
ProCodec <43810146+error9098x@users.noreply.github.com>
2023-11-05 14:47:19 +0000

    hello

---
+++ gaiaspeaks-main/GaiaSpeaksWeb/chat-main/config.js
@@ -0,0 +1,3 @@
+const API_URL = "https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key=AIzaSyAmMtufl_TPyWdLbuCFD5Lh8IEpJWrEXYos";
+const system_message = "Test Prompt!"; 
+const path = "/chat";  
